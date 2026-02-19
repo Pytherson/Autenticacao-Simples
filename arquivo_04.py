222fec1df79a79ac2701d8e4f321e8394548ad2e
Pytherson Mariano <pytherson.dev@gmail.com>
2026-02-19 16:12:20 -0300

    Autenticação 4

---
+++ arquivo_04.py
@@ -0,0 +1,32 @@
+from rich import print
+from rich import inspect
+import os
+
+class ValidadorCadastro:
+    """Autenticação Simples"""
+    def __init__(self):
+        self.__email_correto = "pyther@gmail.com"
+        self.__senha_correta = 123456
+
+
+    def validar_usuario(self):
+        try:
+            email = input("Digite seu email: ")
+            senha = int(input("Digite sua senha: "))
+
+            if email.lower().strip() == self.__email_correto and senha == self.__senha_correta:
+                os.system("cls")
+                print(":white_check_mark:[green] Email e Senha Correta![/]")
+
+            else:
+                os.system("cls")
+                print(":x:[red] Email ou Senha incorreto![/]")
+        except:
+            os.system("cls")
+            print(":thumbs_down:[yellow] Erro: A senha deve conter apenas números![/]")
+
+
+
+v1 = ValidadorCadastro()
+v1.validar_usuario()
+inspect(v1)
